3bdea690b8f7e8be37a7ead380ca233e90cecca4
erizkanr <akuerizka7@gmail.com>
2023-10-13 15:26:59 +0700

    Erizka main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "---------------------------------------------------" << endl << endl;
+    cout << "Nama : Erizka Nia Ramadhani" << endl << endl;
+    cout << "NPM : 2310631170014"<< endl<< endl;
+    cout << "Kelas : B - Informatika"<< endl << endl;
+    cout << "Cita - Cita : CEO"<< endl << endl;
+    cout << "Motivasi Masuk Informatika : rekomendasi dari kakak"<< endl;
+    cout << "---------------------------------------------------"<< endl;
+
+    return 0;
+}
